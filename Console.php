7ae88b80a8cac84ed94deb7dae623ceb9674a83b
Cory Thompson <coryjthompson@gmail.com>
2013-04-08 17:41:04 +1000

    Initial Commit

---
+++ Console.php
@@ -0,0 +1,35 @@
+<?php
+/* php-console-color
+ * Author: Cory Thompson (CoryJThompson)
+ * https://github.com/coryjthompson/php-console-color
+ */
+class Console {
+
+	public static function write($text, $color = null){
+		if($color != null){
+			return print "\033[".$color."m" . $text . "\033[37m";
+		}
+		return print $text;
+	}
+
+	public static function writeLine($text, $color = null){
+		return Console::write($text."\n", $color);
+	}
+
+	public static function writeTitle($title, $color = null){
+		Console::writeLine("---------------------------", $color);
+		Console::writeLine($title, $color);
+		Console::writeLine("---------------------------", $color);
+	}
+}
+
+class ConsoleColor {
+	const black = '30';
+	const red = '31';
+	const green = '32';
+	const brown = '33';
+	const blue = '34'; 
+	const purple = '35';
+	const cyan = '36';
+	const white = '37';
+}
